ed5c53332d7c485112346286c568c06d91f7196d
Ivan Lago <ivanslago@gmail.com>
2024-09-26 18:46:38 -0300

    feat: add initial barershopitem component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,9 +5,13 @@ import Header from "../_components/header"
 import { ptBR } from "date-fns/locale"
 import Search from "./_components/search"
 import BookingItem from "../_components/booking-item"
+import { db } from "../_lib/prima"
+import BarbershopItem from "./_components/barbershop-item"
 
-const name = "test"
-export default function Home() {
+export default async function Home() {
+  // chamar prisma e pegar barbearias
+  const Barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -30,6 +34,17 @@ export default function Home() {
           AGENDAMENTOS
         </h2>
         <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className="mb-3 px-5 text-xs font-bold uppercase text-gray-400">
+          RECOMENDADOS
+        </h2>
+        <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+          {Barbershops.map((barbershp) => (
+            <BarbershopItem key={barbershp.id} Barbershop={barbershp} />
+          ))}
+        </div>
       </div>
     </div>
   )
