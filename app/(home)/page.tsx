3385b43269e93c67972a48b27b0ca6305a11fb7d
Ivan Lago <ivanslago@gmail.com>
2024-09-26 13:51:37 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,22 @@
+import { format } from "date-fns"
 import Image from "next/image"
 import { Button } from "../_components/ui/button"
 import Header from "../_components/header"
+import { ptBR } from "date-fns/locale"
 
 const name = "test"
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel</h2>
+        <p className="text-sm">
+          {format(new Date(), "EEEE',' d ' de ' MMMM ' de ' yyyy", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   )
 }
