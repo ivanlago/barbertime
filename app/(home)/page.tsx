782a1384fa0d7f7a0317a855da4bdbedf5e266d7
Ivan Lago <ivanslago@gmail.com>
2024-09-26 13:09:41 -0300

    feat: add initial header component

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import { Button } from "../_components/ui/button"
+import Header from "../_components/header"
+
+const name = "test"
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image"
-import { Button } from "./_components/ui/button"
-
-const name = "test"
-export default function Home() {
-  return <Button>Test</Button>
-}
