c3deab68b0c53f116ac7f3fb0efd37f6e04d8ce1
Ivan Lago <ivanslago@gmail.com>
2024-09-27 22:00:08 -0300

    feat: add_nextauth_setup

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,22 @@
+"use client"
+
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Header = () => {
+  const { data } = useSession()
+
+  const handleLoginClick = async () => {
+    await signIn()
+  }
+
+  const handleLogoutClick = async () => {
+    await signOut()
+  }
+
   return (
     <Card>
       <CardContent className="flex flex-row items-center justify-between p-5">
@@ -13,6 +26,16 @@ const Header = () => {
           height={22}
           width={120}
         />
+
+        {data?.user ? (
+          <div>
+            <h1>{data.user.name}</h1>
+            <Button onClick={handleLogoutClick}>Logout</Button>
+          </div>
+        ) : (
+          <Button onClick={handleLoginClick}>Login</Button>
+        )}
+
         <Button variant="outline" size="icon" className="h-8 w-8">
           <MenuIcon size={18} />
         </Button>
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,8 +7,8 @@ const handler = NextAuth({
   adapter: PrismaAdapter(db),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 })
