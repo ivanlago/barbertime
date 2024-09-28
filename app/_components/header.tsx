8433f6d5d59380db71f46ba3259115d960f44a65
Ivan Lago <ivanslago@gmail.com>
2024-09-28 16:32:26 -0300

    feat: add signin and signup

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,19 +3,28 @@
 import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react"
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  UserIcon,
+} from "lucide-react"
 import { signIn, signOut, useSession } from "next-auth/react"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet"
+import { Avatar } from "./ui/avatar"
+import { AvatarImage } from "@radix-ui/react-avatar"
+import Link from "next/link"
 
 const Header = () => {
   const { data } = useSession()
-
-  const handleLoginClick = async () => {
-    await signIn()
-  }
-
-  const handleLogoutClick = async () => {
-    await signOut()
-  }
 
   return (
     <Card>
@@ -27,18 +36,84 @@ const Header = () => {
           width={120}
         />
 
-        {data?.user ? (
-          <div>
-            <h1>{data.user.name}</h1>
-            <Button onClick={handleLogoutClick}>Logout</Button>
-          </div>
-        ) : (
-          <Button onClick={handleLoginClick}>Login</Button>
-        )}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="h-8 w-8">
+              <MenuIcon size={18} />
+            </Button>
+          </SheetTrigger>
 
-        <Button variant="outline" size="icon" className="h-8 w-8">
-          <MenuIcon size={18} />
-        </Button>
+          <SheetContent className="p-0">
+            <SheetHeader className="border-b border-solid border-secondary p-5 text-left">
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+
+            {data?.user ? (
+              <div className="flex items-center justify-between px-5 py-6">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    {data.user?.image ? (
+                      <AvatarImage src={data.user?.image} />
+                    ) : (
+                      <UserIcon />
+                    )}
+                    {/* <AvatarImage src={data.user?.image ?? ""} /> */}
+                  </Avatar>
+                  <h2 className="font-bold">{data.user.name}</h2>
+                </div>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  onClick={() => signOut()}
+                >
+                  <LogOutIcon />
+                </Button>
+              </div>
+            ) : (
+              <div className="flex flex-col gap-2 px-5 py-6">
+                <div className="flex items-center gap-3">
+                  <UserIcon />
+                  <h2 className="font-bold">Olá, faça seu login</h2>
+                </div>
+
+                <Button
+                  variant="secondary"
+                  className="w-full justify-start"
+                  onClick={() => signIn("google")}
+                >
+                  <LogInIcon className="mr-2" size={18} />
+                  Fazer Login
+                </Button>
+              </div>
+            )}
+
+            <div className="flex flex-col gap-3 px-5">
+              <Button
+                variant="secondary"
+                className="w-full justify-start"
+                asChild
+              >
+                <Link href="/">
+                  <HomeIcon className="mr-2" size={18} />
+                  Inicio
+                </Link>
+              </Button>
+
+              {data?.user && (
+                <Button
+                  variant="secondary"
+                  className="w-full justify-start"
+                  asChild
+                >
+                  <Link href="/bookings">
+                    <CalendarIcon className="mr-2" size={18} />
+                    Agendamentos
+                  </Link>
+                </Button>
+              )}
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   )
