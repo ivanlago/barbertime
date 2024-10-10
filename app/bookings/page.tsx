15d042a8009d690475df3c9e6a66087fc8d992a8
Ivan Lago <ivanslago@gmail.com>
2024-10-10 19:30:35 -0300

    feat: add postinstall prisma generate

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,9 +1,9 @@
 import { getServerSession } from "next-auth"
 import BookingItem from "../_components/booking-item"
 import Header from "../_components/header"
-import { authOptions } from "../api/auth/[...nextauth]/route"
 import { redirect } from "next/navigation"
 import { db } from "../_lib/prisma"
+import { authOptions } from "../_lib/auth"
 
 const BookingsPage = async () => {
   const session = await getServerSession(authOptions)
